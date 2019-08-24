4e2e5794b8f5173d3939073fd7430d97140927fb
mattxreality <mattxreality@gmail.com>
2019-08-24 15:48:32 -0400

    LWRP test

---
+++ Assets/Scripts/TurnTable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnTable : MonoBehaviour
+{
+
+    public float rotationSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * (rotationSpeed * Time.deltaTime));
+    }
+}
